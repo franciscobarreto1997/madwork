fe7554a9ffa7787f34d7db3076f6f132e06b4aaf
francisco <franciscoantunesbarreto1997@gmail.com>
2020-03-10 15:45:46 +0000

    main text and search component only show up after intro finishes

--- app/javascript/components/Intro.jsx
+++ app/javascript/components/Intro.jsx
@@ -1,7 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
-const Intro = () => {
-
+const Intro = (props) => {
 
   const [buttonAnimated, setButtonAnimated] = useState(false)
   const [divAnimated, setDivAnimated] = useState(false)
@@ -9,22 +8,23 @@ const Intro = () => {
   const button = document.querySelector('button')
   const div = document.querySelector('.intro')
 
-
-  if (button) {
-    if (buttonAnimated === true) {
+  if (buttonAnimated === true) {
+    if (button !== null) {
       setTimeout(() => {
         button.parentNode.removeChild(button);
         setDivAnimated(true)
       }, 1000);
+    }
 
+    if (div !== null) {
       setTimeout(() => {
         div.parentNode.removeChild(div);
+        props.handleChange()
       }, 2200);
     }
   }
 
-
-  return(
+  return (
     <div className={ divAnimated ? "intro div-animation" : "intro" }>
       <h1>MADWORK</h1>
       <button className={buttonAnimated ? "button btn-animation" : "button"} onClick={() => setButtonAnimated(true) }>MADWORK</button>
